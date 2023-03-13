50cc7f684b281a1ea55d5bbba5e03d353f4933a6
Mikaeil <mikaeileghbal@yahoo.com>
2023-03-13 22:31:30 +0330

    Conver to class

--- code/wordcount.js
+++ code/wordcount.js
@@ -1,60 +1,58 @@
-export default function WordCounter() {}
+export default class WordCounter {
+  constructor() {}
 
-WordCounter.prototype.findText = function (text, term) {
-  let pattern = new RegExp(term, "gi");
-  return text.replace(pattern, (matchedText) => {
-    return `<mark>${matchedText}</mark>`;
-  });
-};
-
-WordCounter.prototype.replaceText = function replaceText(
-  text,
-  findTerm,
-  replaceTerm
-) {
-  let pattern = new RegExp(findTerm, "gi");
-  return text.replace(pattern, replaceTerm);
-};
-
-WordCounter.prototype.removeExtraSpaces = function (text) {
-  return text.replace(/\s+/g, " ");
-};
-
-WordCounter.prototype.numberOfCharsWithSpace = function (text) {
-  return text.length;
-};
-
-WordCounter.prototype.numberOfNoSpace = function (text) {
-  let wordsPattern = /\S/g;
-  const result = text.match(wordsPattern);
-  return result ? result.length : 0;
-};
-
-WordCounter.prototype.numberOfWords = function (text) {
-  let wordsPattern = /\b\S+\b/g;
-  const result = text.match(wordsPattern);
-  return result ? result.length : 0;
-};
-
-WordCounter.prototype.numberOfSentences = function (text) {
-  let wordsPattern = /\./g;
-  const result = text.match(wordsPattern);
-  return result ? result.length : 0;
-};
-
-WordCounter.prototype.countWords = function (text) {
-  let wordsPattern = /\b\S+\b/g;
-
-  let wordCount = 0;
-  let wordDictionary = {};
-  for (let word of text.matchAll(wordsPattern)) {
-    wordCount++;
-
-    if (wordDictionary[word]) {
-      wordDictionary[word]++;
-    } else {
-      wordDictionary[word] = 1;
-    }
+  findText(text, term) {
+    let pattern = new RegExp(term, "gi");
+    return text.replace(pattern, (matchedText) => {
+      return `<mark>${matchedText}</mark>`;
+    });
   }
-  return [wordCount, wordDictionary];
-};
+
+  replaceText(text, findTerm, replaceTerm) {
+    let pattern = new RegExp(findTerm, "gi");
+    return text.replace(pattern, replaceTerm);
+  }
+
+  removeExtraSpaces(text) {
+    return text.replace(/\s+/g, " ");
+  }
+
+  numberOfCharsWithSpace(text) {
+    return text.length;
+  }
+
+  numberOfNoSpace(text) {
+    let wordsPattern = /\S/g;
+    const result = text.match(wordsPattern);
+    return result ? result.length : 0;
+  }
+
+  numberOfWords(text) {
+    let wordsPattern = /\b\S+\b/g;
+    const result = text.match(wordsPattern);
+    return result ? result.length : 0;
+  }
+
+  numberOfSentences(text) {
+    let wordsPattern = /\./g;
+    const result = text.match(wordsPattern);
+    return result ? result.length : 0;
+  }
+
+  countWords(text) {
+    let wordsPattern = /\b\S+\b/g;
+
+    let wordCount = 0;
+    let wordDictionary = {};
+    for (let word of text.matchAll(wordsPattern)) {
+      wordCount++;
+
+      if (wordDictionary[word]) {
+        wordDictionary[word]++;
+      } else {
+        wordDictionary[word] = 1;
+      }
+    }
+    return [wordCount, wordDictionary];
+  }
+}
